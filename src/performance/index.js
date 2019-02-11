127b911c463b8efb8008fb5fb024f4c3bc1fb62d
jrm8732 <jrmiller@clarkstontech.com>
2019-02-11 16:00:35 -0500

    added shelves

--- src/performance/index.js
+++ src/performance/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Shelf from './shelf'
 import "./performance.css"
 class Performance extends Component{
     render(){
@@ -6,7 +7,16 @@ class Performance extends Component{
             <div className="performance-main parent">
                 <h1>Performance</h1>
                 <section className='performance-components child'>
-                    welcome
+                    <div className="shelf-row">
+                        <Shelf width="10%" top='20em' left='60em'/>
+                    </div>
+                    <div className="shelf-row">
+                        <Shelf width="60%" top="35em" left ="20em"/>
+                    </div>
+                    <div className="shelf-row">
+                        <Shelf width="40%" top="50em" left="25em"/>
+                    </div>
+                    
                 </section>
             </div>
         )
