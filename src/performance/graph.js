83705eaf2e1ab956b40534643078318a3f718325
jrm8732 <jrmiller@clarkstontech.com>
2019-02-13 09:45:10 -0500

    more graphs

--- src/performance/graph.js
+++ src/performance/graph.js
@@ -1,17 +1,27 @@
 import React from 'react'
 
 const Graph=(props)=>{
+    console.log(props.graph)
     return(  
         <div className="graph-wrapper">
-        <svg width="150" height="115" className="graph">
+        <h5>{props.graph.title}</h5>
+        <h6>{props.graph.sub}</h6>
+        <svg width="125" height="115" className="graph">
             <g transform="translate(10,0)">
-                <rect class="bar" x="22" width="15" y="65" height="50"></rect> 
+                
+                <text x='66' y='22'></text>
+                <rect class="bar" x="22" width="20" y={props.graph.bar1.y} height={props.graph.bar1.height}></rect> 
+                <text dx='2' fontSize='10' y="115" width="40" fill='white' x='22'>Y16</text>
             </g>
             <g transform="translate(20,0)" >
-                <rect class="bar" x="42" width="15" y="45" height="70"></rect> 
+                <text x='66' y='22'></text>
+                <rect class="bar" x="42" width="20" y={props.graph.bar2.y} height={props.graph.bar2.height}></rect> 
+                <text dx='2' fontSize='10' y="115" width="40" fill='white' x='42'>Y17</text>
             </g>
             <g transform="translate(30,0)">
-                <rect class="bar" x="62" width="15" y="25" height="90"></rect> 
+                <text x='66' y='22'></text>
+                <rect class="bar" x="62" width="20" y={props.graph.bar3.y} height={props.graph.bar3.height}></rect> 
+                <text  dx='2 'fontSize='10' y="115" width="40" fill='white' x='62'>Y18</text>
             </g>
         </svg>
         </div>
--- src/performance/index.js
+++ src/performance/index.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Shelf from './shelf'
 import Graph from './graph'
 import "./performance.css"
-class Performance extends Component{
-    render(){
-        return(
+class Performance extends Component {
+    state={
+       group1:[{bar1:{y:'57.39', height:'57.61'}, bar2:{y:'55.27',height:'59.73'},bar3:{y:'53.7', height:'61.24'},title:'NET SALES',sub:'($ Millions)',a:'$5,761',b:'$5,973',c:'$6,124'},
+               {bar1:{y:'50.2', height:'64.8'}, bar2:{y:'44.7',height:'70.3'},bar3:{y:'32.7', height:'82.3'},title:'EARNINS FROM CONTINUED OPERATIONS',sub:'($ Millions)',a:'$648',b:'$703', c:'$823'},
+               {bar1:{y:'22.5', height:'92.5'}, bar2:{y:'21.5',height:'93.5'},bar3:{y:'22.5', height:'92.5'},title:'EBIT MARGIN',sub:'(as a % of Net Sales) (Non-GAAP)',a:'18.5%',b:'18.7%',c:'18.5%'}]
+    }
+    render() {
+        return (
             <div className="performance-main parent">
                 <h1>Performance</h1>
                 <section className='performance-components child'>
                     <div className="shelf-row sr1">
-                        <Graph/>
-                        <Shelf width="100%" top='115px' left='960px;'/>
+                        {/* <Graph /> */}
+                        <Shelf width="100%" top='115px' left='960px' />
                     </div>
                     <div className="shelf-row sr2">
-                        <Graph/>
-                        <Graph/>
-                        <Graph/>
-                        <Shelf width="100%" top="115px" left ="0"/>
+                        {this.state.group1.map(graph=><Graph graph={graph}/>)}
+                        <Shelf width="100%" top="115px" left="0" />
                     </div>
                     <div className="shelf-row sr3">
-                        <Graph/>
-                        <Graph/>
-                        <Graph/>
-                        <Shelf width="100%" top="115px" left="0"/>
+                        {/* <Graph />
+                        <Graph />
+                        <Graph /> */}
+                        <Shelf width="100%" top="115px" left="0" />
                     </div>
-                    
+
                 </section>
             </div>
         )
